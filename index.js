3a011b32b4f30e5feadff1cd25d0827ebf385a84
NoahSolis <NSolis1993@gmail.com>
2022-02-24 01:31:20 -0600

    commit tree 5a04624ec98a2ed6fc8422fb155ceb0c38862d09

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+const returnFirstTwoDrivers = function () {
+    return drivers.slice(0, 2);
+}
+
+const returnLastTwoDrivers = function () {
+    return drivers.slice(-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(money) {
+    return function () {
+        return money * money
+    }
+}
+
+const fareDoubler = money => money * 2;
+
+const fareTripler = money => money * 3;
+
+const selectDifferentDrivers = (drivers, returnFirstTwoDrivers) => {
+    return returnFirstTwoDrivers(drivers);
+}
